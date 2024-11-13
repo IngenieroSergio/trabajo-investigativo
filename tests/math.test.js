0d9f4b23d5f0a3d0fa3277a577b038d4572c76e5
Sergio Novoa <sergio.novoa1109@gmail.com>
2024-11-12 22:03:48 -0500

    prueba correcta

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,7 @@
 const multiply = require('../src/math');
 
 test('Multiplying 2 and 3 should return 6', () => {
-  expect(multiply(2, 3)).toBe(4);
+  expect(multiply(2, 3)).toBe(6);
 });
 
 test('Multiplying 4 and -5 should return -20', () => {
